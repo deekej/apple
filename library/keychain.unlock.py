8f06010abb3af21c5219f88f65b677b183516f47
Dee'Kej <devel@deekej.io>
2022-11-15 16:30:56 +0100

    Initial commit of keychain.unlock module

---
+++ library/keychain.unlock.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python
+
+# Copyright: (c) 2022, Dee'Kej <devel@deekej.io>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+DOCUMENTATION = r'''
+---
+module: keychain.unlock
+
+short_description: Unlocks security keychain on macOS machine
+
+version_added: "1.0.0"
+
+description: This module is used to unlock security keychain on macOS
+             machine, so follow up macOS tasks can run...
+
+options:
+    password:
+        description: Password for unlocking the macOS security keychain.
+        required: true
+        type: raw
+
+    keychain:
+        description:
+          - Path to a non-default security keychain to unlock.
+          - Default: None (system default is used)
+        required: false
+        type: str
+
+author:
+    - Dee'Kej (@deekej)
+'''
+
+EXAMPLES = r'''
+- name: Unlock non-default macOS security keychain
+  unlock.keychain:
+    password:     "{{ keychain_password }}"
+    keychain:     /Users/deekej/Library/Keychains/custom-keychain.db
+  no_log:         true
+'''
+
+# =====================================================================
+
+import subprocess
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.basic import env_fallback
+
+# ---------------------------------------------------------------------
+
+def run_module():
+    # Ansible Module arguments initialization:
+    module_args = dict(
+        password = dict(type='raw', required=True, no_log=True),
+        keychain = dict(type='str', required=False, default=None)
+    )
+
+    # Parsing of Ansible Module arguments:
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=False
+    )
+
+    password = module.params['password']
+    keychain = module.params['keychain']
+
+    result = dict(
+        changed  = False,
+        password = '[REDACTED]',
+        keychain = keychain
+    )
+
+    # -----------------------------------------------------------------
+
+    cmd = [
+        'security',
+        'unlock-keychain',
+        '-p',
+        password
+    ]
+
+    if keychain:
+        cmd.append(keychain)
+
+    # Run the actual unlocking of keychain:
+    try:
+        subprocess.run(cmd, capture_output=True, check=True, text=True, encoding='ascii')
+    except subprocess.CalledProcessError as ex:
+        if ex.returncode != 51:
+            module.fail_json(msg=ex.stderr.rstrip(), **result)
+
+        if keychain:
+            error_msg = "incorrect passphrase for keychain: %s" % keychain
+        else:
+            error_msg = 'incorrect passphrase for default keychain'
+
+        module.fail_json(msg=error_msg, **result)
+    except Exception as ex:
+        module.fail_json(msg=str(ex), **result)
+
+    result['changed'] = True
+    module.exit_json(**result)
+
+# =====================================================================
+
+def main():
+    run_module()
+
+
+if __name__ == '__main__':
+    main()
